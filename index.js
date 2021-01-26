940c1ac0f6b7fc05e96990b7ca69c4e4e6707726
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-01-26 13:48:35 -0500

    commit tree c430784af5d6cc6565a6439d9336dee085dd76c2

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+var customerName = "bob";
+const leastFavoriteCustomer = "jill";
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+  return customerName;
+}
+
+function setBestCustomer() {
+  return (bestCustomer = "not bob");
+}
+
+function overwriteBestCustomer() {
+  return (bestCustomer = "maybe bob");
+}
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "bob";
+}
